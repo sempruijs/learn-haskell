9ac1d9a4be58862def7c4644674cdab0920e08b4
Sem Pruijs <sem@pruijs.nl>
2022-09-22 17:38:55 +0200

    update listComprehension.hs

--- baby.hs
+++ baby.hs
@@ -1,7 +1,11 @@
 doubleMe x = x+ x
 doubleUs x y = doubleMe x + doubleMe y
-doubleSmallNumber x = if x > 100
+doubleSmallNumber x = if x > 100    
     then x
     else x * 2
 doubleSmallNumber' x = (if x < 100 then x * 2 else x) + 1
+
+
+
+
 
---
+++ listComprehension.hs
@@ -0,0 +1,2 @@
+parabol min max = [ x*x | x <- [min..max]]
+rightTriangles = [(a,b,c) | c <- [1..], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
