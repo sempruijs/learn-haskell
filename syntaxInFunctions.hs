445ea16f939f0049a219196f9460f8e136abb7dc
Sem Pruijs <sem@pruijs.nl>
2022-10-01 11:59:07 +0200

    Add shortAdvice function

--- syntaxInFunctions.hs
+++ syntaxInFunctions.hs
@@ -5,3 +5,9 @@ third (_,_,c) = c
 
 length' [] = 0
 length' (_:xs) = 1 + length' xs
+
+shortsAdvice degrees
+    | degrees <= 0 = "It's freezing absolutely not!"
+    | degrees <= 20 = "Nha to cold"
+    | degrees <= 26 = "Today is a good day for shorts"
+    | degrees > 26 = "Go on, but do not forget the sunscreen."
