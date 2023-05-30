84b2302cca682ffcdaa2fe336219f09d593a49e4
sempruijs <sem@pruijs.net>
2023-05-30 14:29:51 +0200

    finish higher order functions

--- higher-order-functions.hs
+++ higher-order-functions.hs
@@ -9,5 +9,32 @@ zipWith' f (x:xs) (y:ys) = (f x y) : zipWith' f xs ys
 flip' :: (a -> b -> c) -> b -> a -> c
 flip' f a b = f b a
 
-flipZip :: [b] -> [a] -> [c]
-flipZip x y = flip' (zip) x y
+sum' :: (Num a) => [a] -> a
+sum' = foldl (+) 0
+
+product' :: (Num a) => [a] -> a
+product' = foldl (*) 1
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldl (\f x -> if x == y then True else f) False ys
+
+maximum' :: (Ord a) => [a] -> a
+maximum' ys = foldl1 (\acc x -> if x > acc then x else acc) ys
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+
+esrever :: [a] -> [a]
+esrever [] = []
+esrever xs = (last xs) : esrever (init xs)
+
+sqrtSum :: Int
+sqrtSum = length (takeWhile (<10000) sqrtSumList) + 1
+  where 
+    sqrtList = [sqrt x | x <- [1..]]
+    sqrtSumList = scanl1 (+) sqrtList
+
+mapOverFunctions = map ($ 4) $ map (*) [sqrt x | x <- [1..100]]
+
+makeNegative :: (Num a) => [a] -> [a]
+makeNegative = map (negate . abs)
