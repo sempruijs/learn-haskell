de17df099ad3b24dbb0364ab73c6fd50bedcc67a
sem pruijs <19208988+sempruijs@users.noreply.github.com>
2023-11-02 17:15:47 +0100

    learn more about type classes

--- types.hs
+++ types.hs
@@ -5,3 +5,16 @@ data Shape = Circle Float | Rectangle Float Float deriving Show
 surface :: Shape -> Float
 surface (Circle r) = r * r * pi 
 surface (Rectangle l1 l2) = l1 * l2
+
+data TraficLight = Red | Orange | Green
+
+instance Eq TraficLight where
+  Red == Red = True
+  Orange == Orange = True
+  Green == Green = True
+  _ == _ = False
+
+instance Show TraficLight where
+  show Red = "Red light"
+  show Orange = "Orange light"
+  show Green = "Green light"
